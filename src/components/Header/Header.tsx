aca21b04b51b006e2ac46ed4e112befd3c04db74
DevCoderGod <devcodergod@gmail.com>
2023-06-03 12:46:03 +0300

    setToken from Header.input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { observer } from 'mobx-react-lite'
 import S from './Header.module.scss'
 import cn from 'classnames'
+import { Store } from '../../Store/store'
 
-export const Header = () => {
+export const Header = observer(function Header(){
+
+	const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+		Store.setToken(e.target.value)
+	}
+
 	return(
 		<div className={cn(S.header)}>
 			<h2>ToolKit</h2>
 			<label htmlFor='token'>Токен:</label>
-			<input id='token'/>
+			<input
+				id='token'
+				value={Store.token}
+				onChange={e => onChange(e)}
+			/>
 		</div>
 	)
-}
+})
