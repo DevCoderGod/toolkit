18afff3c77b0e9d045c5a189351fb0ee730f66c4
DevCoderGod <devcodergod@gmail.com>
2023-06-03 12:44:48 +0300

    add Store

---
+++ src/Store/store.ts
@@ -0,0 +1,49 @@
+import { makeObservable, observable, action } from 'mobx'
+import { IRepository } from '../types/Repository'
+
+export class CAppStore{
+	token: string
+	search: string
+	reporitories: IRepository[]
+	repo: IRepository | null
+
+	constructor() {
+		makeObservable(this, {
+			token: observable,
+			search: observable,
+			reporitories: observable,
+			repo: observable,
+			setToken:action,
+			setReporitories:action,
+			setRepo:action,
+        })
+		this.token = localStorage.getItem("token") ?? ""
+		this.search = ""
+		this.reporitories = []
+		this.repo = null
+	}
+
+	setToken = (t:string) => {
+		localStorage.setItem("token",t)
+		this.token = t
+	}
+
+	setSearch = (s:string) => {
+		localStorage.setItem("search",s)
+		this.search = s
+	}
+
+	setRepo = (r:IRepository) => {
+		this.repo = r
+	}
+
+	setReporitories = (rs:IRepository[]) => {
+		this.reporitories = rs
+	}
+}
+
+export const Store = new CAppStore()
+
+// eslint-disable-next-line
+// @ts-ignore
+globalThis.Store=Store
