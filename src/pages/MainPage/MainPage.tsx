c499af5c1995048d983e779485d9cadc00f84133
DevCoderGod <devcodergod@gmail.com>
2023-06-03 15:33:32 +0300

    set List of repositories from current user

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { observer } from 'mobx-react-lite'
 import S from './MainPage.module.scss'
 import cn from 'classnames'
 import { Search } from './Search/Search'
 import { List } from './List/List'
 import { Paginator } from './Paginator/Paginator'
+import { Store } from '../../Store/store'
+import { api } from '../../Api/Api'
 
-export const MainPage = () => {
+export const MainPage = observer(function MainPage(){
+
+	useEffect(()=>{
+		async function getRepositories(){
+			Store.setReporitories(await api.currentUserRepositories(Store.token))
+		}
+
+		if(Store.token.length>0)getRepositories()
+	},[Store.token])
+
 	return(
 		<div className={cn(S.main)}>
 			<Search/>
@@ -13,4 +25,4 @@ export const MainPage = () => {
 			<Paginator/>
 		</div>
 	)
-}
+})
